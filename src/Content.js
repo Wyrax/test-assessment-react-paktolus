542bb4087cd5563898266aa29cee457f3be98ac4
Wyrax <wyrax@ukr.net>
2021-03-28 10:29:48 +0300

    Basic Header

--- src/Content.js
+++ src/Content.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Icon from '@material-ui/core/Icon';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     '& > *': {
+//       margin: theme.spacing(1),
+//     },
+//   },
+// }));
 
 function Content() {
 
+  // const classes = useStyles();
+
   return (
     <div>
+    {/* <div className={classes.root}> */}
       <h3>Content</h3>
+      <Button variant="contained" color="primary" startIcon={<DeleteIcon />} endIcon={<Icon>send</Icon>}>Material UI</Button>
     </div>
   );
 
--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import React, { Component } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Box from '@material-ui/core/Box';
 
-function Header() {
+function Header() { // Avatar must be replaced by module login/avatar
 
   return (
-    <div>
-      <h2>Header</h2>
+    <div style={{ width: '100%', alignItems: 'center' }}>
+      <Box display="flex" textAlign="left" p={1} bgcolor="background.paper">
+        <Box display="flex" p={1} flexGrow={1} style={{ alignItems: 'center' }}>
+          <img src={logo} style={{ height: '50px'}}></img>
+        </Box>
+        <Box display="flex" p={1} style={{ alignItems: 'center' }}>
+          $42.00
+        </Box>
+        <Box display="flex" p={1} style={{ alignItems: 'center' }}>
+          <Avatar style={{ backgroundColor: 'red' }}>U</Avatar>
+        </Box>
+      </Box>
     </div>
   );
 
